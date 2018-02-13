9e48aa0d62ab7e19baa9a367c18eb5ad3616684c
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-13 10:51:40 -0800

    removed meteor template html

--- client/main.js
+++ client/main.js
@@ -1,22 +1,16 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../imports/ui/containers/App";
+import "./main.css";
 
-import './main.html';
+import { Meteor } from "meteor/meteor";
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+Meteor._debug = (function(super_meteor_debug) {
+  return function(error, info) {
+    if (!(info && _.has(info, "msg"))) super_meteor_debug(error, info);
+  };
+})(Meteor._debug);
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.getElementById("app-root"));
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,92 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
+import "./../imports/startup/register_api";
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  let numOfConnections = 0;
+
+  Streamy.on("clientMove", (d, s) => {
+    Streamy.broadcast(
+      "movement",
+      { data: { direction: d.data, id: d.id, x: d.data.x, y: d.data.y } },
+      d.id
+    );
+  });
+
+  Streamy.on("DJDie", (d, s) => {
+    Streamy.broadcast("killDJ", { data: { id: d.data.id } }, d.myID);
+  });
+
+  Streamy.on("newChallenger", (d, s) => {
+    if (numOfConnections >= 4) {
+      Streamy.emit("gameFull", {}, Streamy.sockets(d.id));
+      console.log(numOfConnections);
+      console.log("seeeeeeeeeeeeeeessss");
+      return;
+    }
+    numOfConnections++;
+    Streamy.broadcast(
+      "createChallenger",
+      { challenger: { id: d.id, player: { x: d.player.x, y: d.player.y } } },
+      d.id
+    );
+  });
+
+  Streamy.on("createChallengerResponse", (d, s) => {
+    Streamy.emit(
+      "requestChallengers",
+      {
+        challenger: {
+          id: d.id,
+          player: { x: d.player.x, y: d.player.y, alive: d.player.alive }
+        }
+      },
+      Streamy.sockets(d.newChallengerId)
+    );
+  });
+
+  Streamy.on("bulletFire", (d, s) => {
+    Streamy.broadcast(
+      "spawnBullet",
+      { data: { x: d.data.bulletx, y: d.data.bullety, facing: d.data.facing } },
+      d.id
+    );
+  });
+
+  Streamy.on("respawnMe", (d, s) => {
+    Streamy.broadcast(
+      "respawnHim",
+      {
+        data: {
+          x: d.data.x,
+          y: d.data.y,
+          id: d.id
+        }
+      },
+      d.id
+    );
+  });
+  Streamy.on("iWon", (d, s) => {
+    Streamy.broadcast(
+      "heWon",
+      {
+        winner: d.email
+      },
+      d.id
+    );
+  });
+  Streamy.on("ScoreUp", (d, s) => {
+    console.log("increase Score");
+    Streamy.broadcast(
+      "upHisScore",
+      {
+        id: d.id,
+        score: d.score
+      },
+      d.id
+    );
+  });
 });
+
+Meteor.onConnection(() => {});
